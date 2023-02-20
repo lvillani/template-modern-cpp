bd79908027c42d93e0e442cdfdaedc121c46d6b7
Lorenzo Villani <lorenzo@villani.me>
2023-02-20 19:31:05 +0100

    feat: add project_version static library

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
+#include <project_version/project_version.h>
+
 int main() {
     std::cout << "Hello, world." << std::endl;
+
+    std::cout << "Project version:" << std::endl;
+    std::cout << "String: " << project_version() << std::endl;
+    std::cout << "Major: " << project_version_major() << std::endl;
+    std::cout << "Minor: " << project_version_minor() << std::endl;
+    std::cout << "Patch: " << project_version_patch() << std::endl;
+    std::cout << "Tweak: " << project_version_tweak() << std::endl;
+
     return 0;
 }
---
+++ src/project_version/project_version.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <string>
+
+std::string project_version();
+
+unsigned project_version_major();
+
+unsigned project_version_minor();
+
+unsigned project_version_patch();
+
+unsigned project_version_tweak();
