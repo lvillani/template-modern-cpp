7591b3db9cfa125de5e61734b1da8ee90999db28
Lorenzo Villani <lorenzo@villani.me>
2024-01-03 21:11:03 +0100

    feat: arch-sensitive conan bootstrap

--- script/bootstrap.py
+++ script/bootstrap.py
@@ -50,6 +50,7 @@ def setup_conan():
         "--build=missing",
         "--conf", "tools.cmake.cmaketoolchain:generator=Ninja",
         "--output-folder", str(CONAN_DIR),
+        *conan_arch(),
         str(TOP_LEVEL),
     )
     # fmt: on
@@ -57,6 +58,20 @@ def setup_conan():
 
 def conan_profiles() -> typing.List[str]:
     return json.loads(output("conan", "profile", "list", "--format", "json"))
+
+
+def conan_arch() -> typing.List[str]:
+    # "conan profile detect" always reports x86_64 on AMD64 systems even if we are
+    # running inside an x86 VS command prompt. We may want to force the env's arch
+    # instead. Let the default profile guide us in all other cases.
+    if "VSCMD_ARG_TGT_ARCH" not in os.environ:
+        return []
+
+    arch = os.environ["VSCMD_ARG_TGT_ARCH"]
+    if arch == "x64":
+        arch = "x86_64"
+
+    return ["--settings", f"arch={arch}"]
 
 
 def call(*args: str) -> int:
